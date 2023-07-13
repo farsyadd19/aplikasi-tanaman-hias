4e93caecdddf6dbd6968dc713c8055c4ac5ade83
farsyadd19 <farhanrasyad13@gmail.com>
2023-07-13 23:17:32 +0700

    kategori

--- app/src/main/java/com/kelompok1/aplikasitanamanhias/activities/Kategori.java
+++ app/src/main/java/com/kelompok1/aplikasitanamanhias/activities/Kategori.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class KategoriActivity extends AppCompatActivity {
+public class Kategori extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
