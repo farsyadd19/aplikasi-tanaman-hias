a887160f8c44db37f7a67af6fb3238362c8dc4a1
fauzanramaa <fauzaneramadane@gmail.com>
2023-07-17 13:46:27 +0700

    Tambah Act Kategori Daun

---
+++ app/src/main/java/com/kelompok1/aplikasitanamanhias/activities/KategoriDaun.java
@@ -0,0 +1,14 @@
+package com.kelompok1.aplikasitanamanhias.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KategoriDaun extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kategori_daun);
+    }
+}
